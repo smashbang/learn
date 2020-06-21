145ecf0fa9ab322c0d23a115e92e8f8e4aa45d30
smashbang <git@kececi.com>
2020-06-21 11:42:43 +1000

    Changed first.py

--- first.py
+++ first.py
@@ -1,6 +1,23 @@
+# User Input Questions
 name = input("What is your name? ")
 age = input("Whats is your age? ")
-registered = input("Have you registered yet? ")
-print("Your name is:",name)
-print("Your age is:",age)
-print("Registration Status: ",registered)
+registered = input("Have you registered yet? (Yes or No): ")
+
+# Strings
+yes: str = "Yes"
+no: str = "No"
+
+# Evaluation
+if registered == yes:
+    print('Your status is: ', yes)
+    print('Welcome!!!', name)
+    print('')
+    print('Details: ')
+else:
+    print('Your status is: ', no, )
+    exit('You are not registered!!! Exiting')
+
+# Output
+print('Your name is:', name)
+print('Your age is:', age)
+print('Registration Status: ', registered)
