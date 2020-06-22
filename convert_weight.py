7bdd0f65e935469737968e91792358a76fe092d3
smashbang <git@kececi.com>
2020-06-22 17:02:15 +1000

    Update convert_weight.py

--- convert_weight.py
+++ convert_weight.py
@@ -6,10 +6,10 @@ weight_lbs: str = 'lbs'
 
 if scale == weight_kgs:
     weight_kgs: str = input('What is your weight? in lbs ')
-    weight_kgs: float = int(weight_kgs) * 2.204623
+    weight_kgs: float = int(weight_kgs) * 0.4535924
 elif scale == weight_lbs:
     weight_lbs: str = input('What is your weight? in kgs ')
-    weight_lbs: float = int(weight_lbs) * 0.4535924
+    weight_lbs: float = int(weight_lbs) * 2.204623
 
 kgs: str = 'kgs'
 lbs: str = 'lbs'
