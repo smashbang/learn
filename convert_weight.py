4cf1b83422bb0e304aba6e55d0347781624971f8
smashbang <git@kececi.com>
2020-06-22 16:54:45 +1000

    Update convert_weight.py
    Fixed typo..

--- convert_weight.py
+++ convert_weight.py
@@ -1,15 +1,18 @@
 # n
 scale: str = input('What do you want to convert to kgs or lbs? ')
 
+weight_kgs: str = 'kgs'
+weight_lbs: str = 'lbs'
+
+if scale == weight_kgs:
+    weight_kgs: str = input('What is your weight? in lbs ')
+    weight_kgs: float = int(weight_kgs) * 2.204623
+elif scale == weight_lbs:
+    weight_lbs: str = input('What is your weight? in kgs ')
+    weight_lbs: float = int(weight_lbs) * 0.4535924
+
 kgs: str = 'kgs'
 lbs: str = 'lbs'
-
-# i
-weight = input('What is your weight?  ')
-weight_kgs: float = int(weight) * 2.204623
-weight_lbs: float = int(weight) * 0.4535924
-
-
 
 if scale == kgs:
     print('Your weight is: ', weight_kgs, ' kgs')
